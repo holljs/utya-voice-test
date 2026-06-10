8e3cc22a0f758dc04ec49b100fe63387fa85a79d
holljs <117600610+holljs@users.noreply.github.com>
2026-06-10 22:09:17 +0300

    Update test_voice.py

--- test_voice.py
+++ test_voice.py
@@ -9,7 +9,7 @@ style = tts.get_voice_style(voice_name="F4")
 quiz_phrases = {
     # --- Лайфхаки: Магия на 5 (Твой алгоритм с ноликом) ---
     "magic5_2.wav": "Молодец!",
-    "magic5_3.wav": МОЛОДЕЦ!!",
+    "magic5_3.wav": "МОЛОДЕЦ!!",
     "magic5_4.wav": "Молодееец!",
     "magic5_5.wav": "Молодеец!!!",
     "magic5_6.wav": "Мо-ло-деец!",
@@ -28,7 +28,8 @@ for filename, text in quiz_phrases.items():
             total_steps=10,
             speed=0.7 
         )
-        sf.write(filename, wav.squeeze(), 54000)
+        # Вернули правильную частоту 24000!
+        sf.write(filename, wav.squeeze(), 24000)
     except Exception as e:
         print(f"❌ Ошибка при генерации {filename}: {e}")
 
